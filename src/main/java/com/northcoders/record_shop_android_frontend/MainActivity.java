e25f375c19344ee28135ca4b84acc70294013151
Shamar <Shamar098@gmail.com>
2024-12-17 11:26:13 +0000

    feat: add liveData to MainActivity

--- src/main/java/com/northcoders/record_shop_android_frontend/MainActivity.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.northcoders.record_shop_android_frontend;
 
+import android.app.Application;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,11 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.northcoders.record_shop_android_frontend.model.AlbumRepository;
 
 public class MainActivity extends AppCompatActivity {
 
+    AlbumRepository albumRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Application application = new Application();
+        albumRepository = new AlbumRepository(application);
+        albumRepository.getMutableLiveData();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
