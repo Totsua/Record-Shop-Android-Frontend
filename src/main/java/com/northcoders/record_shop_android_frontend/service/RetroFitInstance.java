94c8ee29ecc7ada4e705be7c7b340fb5ac1fd288
Shamar <Shamar098@gmail.com>
2024-12-17 11:10:38 +0000

    feat: add RetroFitInstance service class

---
+++ src/main/java/com/northcoders/record_shop_android_frontend/service/RetroFitInstance.java
@@ -0,0 +1,26 @@
+package com.northcoders.record_shop_android_frontend.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroFitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/recordshop/";
+
+    public static AlbumApiService getService(){
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
+}
