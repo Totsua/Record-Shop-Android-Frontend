e91e0c5afecf3646dcafd491fab6043430caf2b9
Shamar <Shamar098@gmail.com>
2024-12-19 17:50:44 +0000

    feat: add implementation to button clicks

--- src/main/java/com/northcoders/record_shop_android_frontend/ui/updatealbum/UpdateAlbumClickHandler.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/ui/updatealbum/UpdateAlbumClickHandler.java
@@ -1,9 +1,15 @@
 package com.northcoders.record_shop_android_frontend.ui.updatealbum;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 
 import com.northcoders.record_shop_android_frontend.model.Album;
+import com.northcoders.record_shop_android_frontend.model.Artist;
+import com.northcoders.record_shop_android_frontend.ui.mainactivity.MainActivity;
 import com.northcoders.record_shop_android_frontend.ui.mainactivity.MainActivityViewModel;
+
+import java.util.function.Predicate;
 
 public class UpdateAlbumClickHandler {
     Album album;
@@ -17,9 +23,67 @@ public class UpdateAlbumClickHandler {
         this.context = context;
     }
 
+    Predicate<Album> isAlbumPropertiesValid = albumToCheck -> {
+        if(albumToCheck.getName() == null ||
+                albumToCheck.getArtist().getName().isBlank() ||
+                albumToCheck.getArtist() == null ||
+                albumToCheck.getGenre() == null ||
+                albumToCheck.getReleaseDate() == null ||
+                String.valueOf(albumToCheck.getPrice()) == null ||
+                String.valueOf(albumToCheck.getStock())== null)
+
+        // Fields can be null(?) they can be empty but if empty they will become null?
+            // Backend can ignore null values
+
+        {
+            return true;
+        }
+        return false;
+    };
+
+    public void onUpdateAlbumButtonClick(View view){
+        Album updatedAlbum = new Album();
+        updatedAlbum.setId(album.getId());
+        if(!album.getName().isBlank()){
+            updatedAlbum.setName(album.getName());
+        }
+        if(!album.getArtist().getName().isBlank()){
+            Artist artist = new Artist();
+            artist.setName(updatedAlbum.getName());
+            updatedAlbum.setArtist(artist);
+        }
+        if(!album.getGenre().isBlank()){
+            updatedAlbum.setGenre(album.getGenre());
+        }
+        if(!album.getReleaseDate().isBlank()){
+            updatedAlbum.setReleaseDate(album.getReleaseDate());
+        }
+        if(album.getPrice() > 0){
+            updatedAlbum.setPrice(album.getPrice());
+        }
+        if(album.getStock() > 0){
+            updatedAlbum.setStock(album.getStock());
+        }
 
 
+        viewModel.updateAlbum(updatedAlbum.getId(),updatedAlbum);
+        Intent intent = new Intent(context, MainActivity.class);
+
+        context.startActivity(intent);
+    }
 
 
+    public void onDeleteButtonClick(long id){
+        viewModel.deleteAlbum(album.getId());
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+
+    public void onGoBackButtonClick(View view){
+        Intent intent = new Intent(context,MainActivity.class);
+        context.startActivity(intent);
+
+    }
 
 }
