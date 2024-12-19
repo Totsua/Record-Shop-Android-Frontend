03e6ea10dc21312a56f08913d587703c74a19a06
Shamar <Shamar098@gmail.com>
2024-12-19 11:06:52 +0000

    feat: add PATCH and DELETE requests

--- src/main/java/com/northcoders/record_shop_android_frontend/service/AlbumApiService.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/service/AlbumApiService.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface AlbumApiService {
     @GET("albums")
@@ -15,5 +18,11 @@ public interface AlbumApiService {
 
     @POST("albums/create")
     Call<Album> postAlbum(@Body Album album);
+
+    @PATCH("albums/{id}")
+    Call<Album> updateAlbum(@Path("id") long id,@Body Album album);
+
+    @DELETE("albums/{id}")
+    Call<Void> deleteAlbum(@Path("id") long id);
 
 }
