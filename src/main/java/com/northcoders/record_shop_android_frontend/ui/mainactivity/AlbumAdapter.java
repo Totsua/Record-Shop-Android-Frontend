3ad9dabb38a5d8670928244bf80d835c87b5898a
Shamar <Shamar098@gmail.com>
2025-01-05 22:03:48 +0000

    comment: add todo comment

--- src/main/java/com/northcoders/record_shop_android_frontend/ui/mainactivity/AlbumAdapter.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/ui/mainactivity/AlbumAdapter.java
@@ -45,6 +45,9 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
     @Override
     public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
         Album album = albumList.get(position);
+        //set the images for each album
+        //todo:
+        // Save each image the first time Glide loads it, so it can pull it from memory
         ImageView imageView = holder.binding.image;
         Glide.with(imageView)
                 .load(album.getUrl())
