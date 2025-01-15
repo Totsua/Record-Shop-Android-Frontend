b718aac8c84780c63678a5f358e682c4350df651
Shamar <Shamar098@gmail.com>
2025-01-15 09:44:53 +0000

    feat: add logic for recycler view in FirstFragment

--- src/main/java/com/northcoders/record_shop_android_frontend/ui/fragments/FirstFragment.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/ui/fragments/FirstFragment.java
@@ -1,66 +1,142 @@
 package com.northcoders.record_shop_android_frontend.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewGroup;import android.widget.Toast;
 
 import com.northcoders.record_shop_android_frontend.R;
+import com.northcoders.record_shop_android_frontend.databinding.FragmentFirstBinding;
+import com.northcoders.record_shop_android_frontend.model.Album;
+import com.northcoders.record_shop_android_frontend.ui.mainactivity.AlbumAdapter;
+import com.northcoders.record_shop_android_frontend.ui.mainactivity.MainActivityViewModel;
+import com.northcoders.record_shop_android_frontend.ui.mainactivity.RecyclerViewInterface;
+import com.northcoders.record_shop_android_frontend.ui.updatealbum.UpdateAlbumActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
+public class FirstFragment extends Fragment implements RecyclerViewInterface {
 
+    private RecyclerView recycler;
+    private ArrayList<Album> albums;
+    private ArrayList<Album> filteredAlbumsList;
+    private AlbumAdapter albumAdapter;
+    private MainActivityViewModel viewModel;
+    private FragmentFirstBinding binding;
+    private static final String ALBUM_KEY = "album";
 
-
-public class FirstFragment extends Fragment  {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        getAllAlbums();
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        binding.searchView.clearFocus();
+        binding.searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                filterList(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+
+    }
+
+
+    private void filterList(String text){
+        filteredAlbumsList = new ArrayList<>();
+
+        for(Album album : albums){
+            if(album.getName().toLowerCase().contains(text.toLowerCase())){
+                filteredAlbumsList.add(album);
+            }
         }
+        if(filteredAlbumsList.isEmpty()){
+            Toast.makeText(this.getContext(), "No Albums Found", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            albumAdapter.setFilterList(filteredAlbumsList);
+        }
+    }
+
+    private void getAllAlbums(){
+        viewModel.getAllAlbums().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albumsFromLiveData) {
+                albums = (ArrayList<Album>) albumsFromLiveData;
+                displayInRecyclerView();
+            }
+        });
+    }
+
+    private void displayInRecyclerView(){
+
+        recycler = binding.recycler;
+        // Because this class implements the RecyclerViewInterface ->
+        // "this" can be passed in for the interface parameter
+        albumAdapter = new AlbumAdapter(albums,this.getContext(),this);
+        recycler.setAdapter(albumAdapter);
+        recycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recycler.setHasFixedSize(true);
+        albumAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Intent intent = new Intent(this.getContext(), UpdateAlbumActivity.class);
+        Album chosenAlbum = albums.get(position);
+        // Check log to see if album correctly appears
+        Log.i("Main Activity Choose Album",chosenAlbum.toString());
+
+        if(filteredAlbumsList == null || filteredAlbumsList.isEmpty()){
+            // pass the chosen album to the next activity
+            intent.putExtra(ALBUM_KEY,chosenAlbum);
+        }else{
+            intent.putExtra(ALBUM_KEY,filteredAlbumsList.get(position));
+        }
+
+        startActivity(intent);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_first,container,false);
+        View view = binding.getRoot();
+
+    // Inflate the layout for this fragment
+        return view;
     }
 
 }
