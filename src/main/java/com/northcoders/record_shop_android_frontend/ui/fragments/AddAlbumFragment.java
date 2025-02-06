6e9abcf31317f4f2243170f2fd43f78ff6aec236
Shamar <Shamar098@gmail.com>
2025-02-06 22:53:46 +0000

    feat: set binding and viewmodel for AddAlbumFragment

--- src/main/java/com/northcoders/record_shop_android_frontend/ui/fragments/AddAlbumFragment.java
+++ src/main/java/com/northcoders/record_shop_android_frontend/ui/fragments/AddAlbumFragment.java
@@ -2,65 +2,42 @@ package com.northcoders.record_shop_android_frontend.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.northcoders.record_shop_android_frontend.R;
+import com.northcoders.record_shop_android_frontend.databinding.FragmentAddalbumBinding;
+import com.northcoders.record_shop_android_frontend.ui.mainactivity.MainActivityViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddAlbumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AddAlbumFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MainActivityViewModel viewModel;
+    private FragmentAddalbumBinding binding;
 
     public AddAlbumFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddAlbumFragment newInstance(String param1, String param2) {
-        AddAlbumFragment fragment = new AddAlbumFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_addalbum, container,false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addalbum, container, false);
+        return binding.getRoot();
     }
 }
